a743964675e499d80004b4f15298ea9f348aaa58
nemaded <nemade.d@northeastern.edu>
2023-10-02 15:39:02 -0400

    removed the config file

--- App/config.py
+++ App/config.py
@@ -1,14 +1,14 @@
-from pydantic_settings import BaseSettings
+# from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    database_hostname: str
-    database_password: str
-    database_name: str 
-    database_username: str
-    secret_key: str
-    algorithm: str
-    class Config:
-        env_file= ".env"
+# class Settings(BaseSettings):
+#     database_hostname: str
+#     database_password: str
+#     database_name: str 
+#     database_username: str
+#     secret_key: str
+#     algorithm: str
+#     class Config:
+#         env_file= ".env"
 
 
-settings= Settings()
+# settings= Settings()
