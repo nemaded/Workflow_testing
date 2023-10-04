1ef7a5c1b20d09f36981129f523ae4cc00b1b6b3
nemaded <nemade.d@northeastern.edu>
2023-10-04 09:29:31 -0400

    changed db

--- App/database.py
+++ App/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from .config import settings
+from .config import settings
 
 
 
@@ -9,17 +9,18 @@ import os
 
 
 # Check if we are running in GitHub Actions workflow environment
+
 is_github_actions = os.getenv("CI") == "true"
+print("git hub true")
 
+if is_github_actions:
 
+    POSTGRES_PASSWORD = os.getenv("PGPASSWORD")
+    POSTGRES_HOST =os.getenv("PGHOST")
+    DATABASE_URL = f"postgresql://postgres:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/cloud_bd"
 
-POSTGRES_PASSWORD = os.getenv("PGPASSWORD")
-POSTGRES_DB = os.getenv("PGDATABASE")
-POSTGRES_HOST =os.getenv("PGHOST")
-
-
-DATABASE_URL = f"postgresql://postgres:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/cloud_bd"
-
+else:
+    DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 
 engine = create_engine(
     DATABASE_URL
